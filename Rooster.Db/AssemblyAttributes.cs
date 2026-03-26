7ca14a7b408c2e7d631a04b600f944105737f584
gubard <maksimovsergey08@gmail.com>
2026-03-26 21:33:16 +0200

    Add assembly attributes for `AlarmEntity` to support ADO and LiteDB operations

---
+++ Rooster.Db/AssemblyAttributes.cs
@@ -0,0 +1,8 @@
+﻿using Nestor.Db.LiteDb.Models;
+using Nestor.Db.Models;
+using Rooster.Contract.Models;
+
+[assembly: Ado(typeof(AlarmEntity), nameof(AlarmEntity.Id), false)]
+[assembly: AdoSourceEntity(typeof(AlarmEntity), nameof(AlarmEntity.Id))]
+[assembly: LiteDb(typeof(AlarmEntity), nameof(AlarmEntity.Id), false)]
+[assembly: LiteDbSourceEntity(typeof(AlarmEntity), nameof(AlarmEntity.Id))]
